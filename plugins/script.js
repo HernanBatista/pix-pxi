51896b9a665cb61df3eb29abd6bfff3cb66d3338
Hernan <62160758+hernanb13@users.noreply.github.com>
2021-12-11 18:53:13 -0300

    Add files via upload

--- plugins/script.js
+++ plugins/script.js
@@ -1,8 +1,33 @@
-document.addEventListener('mousemove', function(e) {
-  let body = document.querySelector('body');
-  let circle = document.getElementsByClassName('logo');
-  let left = e.offsetX;
-  let top = e.offsetY;
-  circle.style.left = left + 'px';
-  circle.style.top = top + 'px';
-});
+function getMouseCoords(e) {
+  var e = e || window.event;
+}
+
+
+var followCursor = (function() {
+  var s = document.createElement('div');
+  s.style.position = 'absolute';
+  s.style.margin = '0';
+  s.style.padding = '5px';
+  s.style.backgroundImage = "url('images/pix-pxi.png')";
+  s.style.backgroundSize = "contain";
+  s.style.width = '150px';
+  s.style.height = '150px';
+
+  return {
+    init: function() {
+      document.body.appendChild(s);
+    },
+
+    run: function(e) {
+      var e = e || window.event;
+      s.style.left = (e.clientX - 5) + 'px';
+      s.style.top = (e.clientY - 5) + 'px';
+      getMouseCoords(e);
+    }
+  };
+}());
+
+window.onload = function() {
+  followCursor.init();
+  document.body.onmousemove = followCursor.run;
+}
