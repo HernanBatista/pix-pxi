cb8bc77053ed90ff530d783ca2a6a40240dd147c
Hernan <62160758+hernanb13@users.noreply.github.com>
2021-12-11 15:30:02 -0300

    Add files via upload

--- plugins/script.js
+++ plugins/script.js
@@ -1,29 +1,8 @@
-window.onload = function(){
- document.getElementById("distorcao").style.transform =  "translate(-50%,-50%) rotateX(0deg) rotateY(0deg) translateZ(0)";
-}
-document.onmousemove = function(e){
- x = e.pageX, y = e.pageY;
- limite = 35;
- 
- var ww = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
- var wh = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
- 
- rotx = y*100/wh;
- rotx = 180*rotx/100;
- rotx = rotx-90;
- rotx *= -1;
- 
- roty = x*100/ww;
- roty = 180*roty/100;
- roty = roty-90;
-
- if (roty < -4){
-	 document.getElementById("distorcao").style.transform =  "translate(-50%,-50%) rotateX("+(rotx*limite/100)+"deg) rotateY("+(roty*limite/100)+"deg) translateZ(0)";
-	 document.getElementById("dist").style.transform =  "scaleX(1)";
-	 
- }else if(roty > 4){
-	 document.getElementById("distorcao").style.transform =  "translate(-50%,-50%) rotateX("+(rotx*limite/100)+"deg) rotateY("+(roty*limite/100)+"deg) translateZ(0)";
-	 document.getElementById("dist").style.transform =  "scaleX(-1)";
- }
-
-};
+document.addEventListener('mousemove', function(e) {
+  let body = document.querySelector('body');
+  let circle = document.getElementsByClassName('logo');
+  let left = e.offsetX;
+  let top = e.offsetY;
+  circle.style.left = left + 'px';
+  circle.style.top = top + 'px';
+});
